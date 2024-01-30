fd4d4ce2bba1e9bb749e2af796bb7f17f7b98574
CarlsFencingDecking <158094575+CarlsFencingDecking@users.noreply.github.com>
2024-01-30 12:43:19 -0500

    Update index.js
    Error on other commit. Add alert if "I Accept" but "I Agree" not checked.

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ check.onclick = function(event){
         submit.classList.add('on')
         submit.href = 'https://533330.extforms.netsuite.com/app/site/hosting/scriptlet.nl?script=4050&deploy=1&compid=533330&h=e501275711f091b66330&mesaltid=22214-370834230241501-43e094e4-2&meskey=U2FsdGVkX1%2FOyBB7%2F0D6TgnWKAWy4hs0dMwo%2Bfrxzhhoxt4LppSrMeos%2BwBQMM%2BtN2JbDuOGHG41Hrsl5IVtxg%3D%3D'
     }else {
-        alert('To Proceed Please Agree To The Terms and Conditions.')
+        
         steps[0].style.fontWeight = 600
         steps[1].style.fontWeight = 100
         submit.href = '#';
@@ -23,6 +23,12 @@ check.onclick = function(event){
         submit.classList.remove('on')
     }
 };
+
+submit.onclick = function(event){
+    if(!check.checked){
+        alert('To Proceed Please Agree To The Terms and Conditions.')
+    }
+}
 
 decline.addEventListener('click', function(event){
     window.close()
