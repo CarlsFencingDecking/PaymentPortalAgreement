09289cf809162b71f274b6823e5b042409d25a84
CarlsFencingDecking <158094575+CarlsFencingDecking@users.noreply.github.com>
2024-02-05 09:42:22 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ steps[0].style.fontWeight = 600;
 var url = window.location.href;
 //var url = 'https://carlsfencingdecking.github.io/PaymentPortalAgreement/?param1=$588.60%26param2=$122.17%26param3=$305.43%26param4=$91.63%26param5=$61.09%26param6=$30.54%26param7=https://533330.extforms.netsuite.com/app/site/hosting/scriptlet.nl?script=4050&deploy=1&compid=533330&h=e501275711f091b66330&mesaltid=19432-520760330240202-3a7c8ac9-9&meskey=U2FsdGVkX1%2BRzfspXBsSbmd4tl2j4oR3Ugpm3EaG8404aUyYIT2ySE6dALG34yCMl%2FwZA2jYC6OvcCC38zAMRQ%3D%3D';
 console.log(url)
-url = url.replace('https://carlsfencingdecking.github.io/PaymentPortalAgreement/?', '').replaceAll('%26', '').replaceAll('1=','').replaceAll('2=','').replaceAll('3=','').replaceAll('4=','').replaceAll('5=','').replaceAll('6=','').replaceAll('7=','');
+url = url.replace('https://carlsfencingdecking.github.io/PaymentPortalAgreement/?', '').replaceAll('%26', '').replaceAll('1=','').replaceAll('2=','').replaceAll('3=','').replaceAll('4=','').replaceAll('5=','').replaceAll('6=','').replaceAll('7=','').replaceAll('$', '');
 
 url = url.split('param')
 url.shift();
@@ -49,12 +49,39 @@ decline.addEventListener('click', function(event){
 
 let payPlans = document.querySelectorAll('.payPlans');
 let paySpans = document.querySelectorAll('.paySpans');
+let n = 0;
+let pVal = [];
 
 payPlans.forEach(function(p,i){
-    p.innerText = setPrices[i];
+    p.value = setPrices[i];
 
     if(url[i] === '$0.00' || url[i] === '' || url[i] === undefined || url[i] === null){
         paySpans[i].remove();
     }
+
     
+
+    p.onclick = function(){
+
+        n++;
+        
+        
+        p.select();
+        navigator.clipboard.writeText(p.value);
+        steps[2].style.fontWeight = 600;
+        pVal.unshift(p.value);
+        console.log(pVal)
+
+        if(n === 1){
+            steps[2].innerText = steps[2].innerText+' || Copied Amount : $'+pVal[0];
+        }else if(n > 1){
+            steps[2].innerText = steps[2].innerText.replaceAll(pVal[1], pVal[0])
+        }
+        
+
+
+
+    }
+
+   
 });
